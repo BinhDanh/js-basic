3c7a041ad46a275a1d19e7cebe3effe44d887487
BinhDanh <danhbinh943@gmail.com>
2025-03-08 10:23:14 +0700

    video4

---
+++ video4/video4.js
@@ -0,0 +1,10 @@
+let a = `'I' là string - chuỗi`
+let text = `We are the so-called 
+"Vikings" from the north.`;
+
+// console.log(a)
+// console.log(4+5+'a')
+// console.log('a'+5+4)
+
+console.log(a)
+console.log(text)
